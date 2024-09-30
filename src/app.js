96e082b75a857c6ef957c0988c1574a4f03f90e4
KhoaLee411 <leekhoa7678@gmail.com>
2024-10-01 03:40:08 +0700

    section 2 khoi tao mot project BE

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const morgan = require('morgan')
+const {default: helmet} = require('helmet')
+const compression = require('compression')
+const app = express()
+
+
+// init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+// init db
+
+
+// init routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: 'Welcome Fantipjs!', // function để thông báo
+    })
+})
+
+// handling errors
+module.exports = app
